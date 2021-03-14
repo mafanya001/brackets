a8d94f7b9da8b4320309479f9daf7dfc3297951d
mafanya001 <79634135+mafanya001@users.noreply.github.com>
2021-03-14 12:25:57 +0300

    fix: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = [];
+    let count = str.length;
+    let imputStr = str;
+    result = bracketsConfig.map(item => (item[0] + item[1]));
+
+
+    for (let j = 0; j < count; j++){
+
+
+        for (let i = 0; i < result.length; i++) {
+            str = str.replace(result[i], '');
+        }
+        if (imputStr === str) {
+            j = count;
+        }
+    }
+    if (str.length === 0) {
+        return true;
+    } else return false;
 }
